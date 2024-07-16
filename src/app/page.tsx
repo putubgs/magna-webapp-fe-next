7962862aa43d6303dd19b06b86ab2e4498717bf2
glennhkm <glenn@mhs.usk.ac.id>
2024-07-16 22:38:21 +0700

    chore: cleaning code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
-// import Image from "next/image";
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <div>
-//         Magna Web v1
-//       </div>
-//     </main>
-//   );
-// }
-
-import Section1 from "@/components/section1/main";
+import Image from "next/image";
+import Section1 from '@/components/section1/main';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
+     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
       <div>
+        Magna Web v1
         <Section1/>
       </div>
     </main>
   );
 }
+
