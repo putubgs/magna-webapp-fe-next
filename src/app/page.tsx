e887432253f59050ea346c47c6b209a3550bd980
unknown <leontrg32@gmail.com>
2024-07-16 22:00:34 +0700

    slicing home page stats section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
+import Section3 from "@/components/section-3";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        Magna Web v1
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div>Magna Web v1</div>
+      <Section3 />
     </main>
   );
 }
