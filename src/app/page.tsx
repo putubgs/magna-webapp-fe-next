63fe3f9696a3fa1b026f963c81e6e0472a528796
glennhkm <glenn@mhs.usk.ac.id>
2024-07-16 22:28:16 +0700

    feat: section 1 homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <div>
+//         Magna Web v1
+//       </div>
+//     </main>
+//   );
+// }
+
+import Section1 from "@/components/section1/main";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
       <div>
-        Magna Web v1
+        <Section1/>
       </div>
     </main>
   );
