912cf498ff36380a605423c1780095b1ec52ea9c
alvinfdl21 <alvinfdl21@gmail.com>
2024-07-16 22:38:05 +0700

    feat: about page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import About from "@/components/about/main";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        Magna Web v1
-      </div>
+      <div>Magna Web v1</div>
+      <About />
     </main>
   );
 }
