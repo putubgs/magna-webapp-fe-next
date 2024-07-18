4daf7207a661634da1a4da0bddb3785346f831a9
SeedONova <putubaguswidia@gmail.com>
2024-07-18 20:07:39 +0700

    section 3 responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const gilda = Gilda_Display({ subsets: ["latin"], weight: "400" });
 export default function Home() {
   return (
     <main className="smooth-scroll">
-      <div className="sticky top-0 bg-black z-50 flex justify-between py-6 px-[120px] items-center shadow-md">
+      <div className="sticky top-0 bg-black z-50 flex justify-between py-6 px-4 md:px-[120px] items-center shadow-md">
         <div className="flex items-center space-x-4">
           <Image
             src="/assets/logo/business-units/Magna.png"
@@ -26,9 +26,11 @@ export default function Home() {
             width={35}
             height={56}
           />
-          <div className="text-[24px] font-extrabold">Magna Partners</div>
+          <div className="md:text-[24px] text-[18px] font-extrabold">
+            Magna Partners
+          </div>
         </div>
-        <div className="flex space-x-12">
+        <div className="hidden md:flex space-x-12">
           <Link href="#about" className="cursor-pointer">
             Business Units
           </Link>
@@ -39,11 +41,42 @@ export default function Home() {
             Contact
           </Link>
         </div>
+        <div className="block md:hidden">
+          <svg
+            width="45"
+            height="46"
+            viewBox="0 0 45 46"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              width="20"
+              height="2"
+              transform="translate(12 27)"
+              fill="white"
+            />
+            <rect
+              width="20"
+              height="2"
+              transform="translate(12 16)"
+              fill="white"
+            />
+          </svg>
+        </div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="w-screen h-auto bg-[url('/images/section1-bg.png')] bg-cover px-[120px] pt-24">
+        <div className="relative min-w-screen h-auto px-5 md:px-[120px] pt-24">
+          <div className="absolute bottom-0 -z-10 h-full w-full -mx-5 md:-mx-[120px]">
+            <Image
+              src="/images/section1-bg.png"
+              alt="background-section1"
+              objectFit="cover"
+              fill
+              priority
+            />
+          </div>
           <div className="flex flex-col gap-12 w-full">
-            <h1 className="text-7xl">
+            <h1 className="text-4xl md:text-7xl">
               <p className="mb-3">
                 We're <b>Magna</b>
               </p>
@@ -51,18 +84,18 @@ export default function Home() {
                 Greatness <span className={gilda.className}>Start Here</span>
               </p>
             </h1>
-            <p className="text-lg font-medium">
+            <p className="text-lg font-medium text-white/70">
               Magna Partners was established in 2024 as a collectively developed
               network of organizations and communities that focused on solving
               the problems of youths: high schoolers, college students, and
               young professionals, with various types of products and services.
             </p>
-            <Link className="flex gap-3 items-center" href="#contact">
+            <button className="flex gap-3 items-center">
               <p className="text-lg">Contact Us</p>
               <div className="border border-white/30 p-2 rounded-full">
                 <ArrowIcon height={14} width={14} />
               </div>
-            </Link>
+            </button>
             <Image
               src="/images/section1-hero.png"
               alt="section1-hero"
@@ -84,22 +117,26 @@ export default function Home() {
       </div>
       <div
         id="contact"
-        className={`${styles.background} flex flex-col items-center p-[80px] space-y-[40px]`}
+        className={`${styles.background} flex flex-col items-center md:p-[80px] space-y-[40px] items-center justify-center`}
       >
-        <div className="flex flex-col items-center text-[16px]">
-          <div className="text-[24px]">Contact Us</div>
-          <div className={`${gilda.className} text-[48px]`}>
+        <div className="flex flex-col items-center text-[16px] text-center">
+          <div className="md:text-[24px] text-[16px]">Contact Us</div>
+          <div className={`${gilda.className} md:text-[48px] text-[32px]`}>
             Stay connected with Magna Partners!
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-y-6 gap-x-[100px]">
+        <div className="grid grid-cols-2 gap-y-6 md:gap-x-[100px] gap-x-[15px]">
           <Link className="flex space-x-3 items-center" href="#">
             <div className="flex items-center justify-center rounded-full border border-white w-[30px] h-[30px]">
               <MailIcon size={15} color="white" />
             </div>
-            <div>magna.partners@gmail.com</div>
+            <div>@magna.partners</div>
           </Link>
-          <Link className="flex space-x-3 items-center" href="https://www.instagram.com/magna.partners/" target="_blank">
+          <Link
+            className="flex space-x-3 items-center text-end"
+            href="https://www.instagram.com/magna.partners/"
+            target="_blank"
+          >
             <div className="flex items-center justify-center rounded-full border border-white w-[30px] h-[30px]">
               <InstaIcon size={15} color="white" />
             </div>
@@ -111,7 +148,11 @@ export default function Home() {
             </div>
             <div>+628999009029</div>
           </Link>
-          <Link className="flex space-x-3 items-center" href="https://www.linkedin.com/company/magna-id/" target="_blank">
+          <Link
+            className="flex space-x-3 items-center"
+            href="https://www.linkedin.com/company/magna-id/"
+            target="_blank"
+          >
             <div className="flex items-center justify-center rounded-full border border-white w-[30px] h-[30px]">
               <LinkedinIcon size={15} color="white" />
             </div>
--- src/components/section-3.tsx
+++ src/components/section-3.tsx
@@ -10,81 +10,83 @@ function Section3() {
         <Image src={"/assets/impact-img.png"} alt="Background Image" fill />
       </div>
 
-      <div className="px-[120px] py-[96px] flex flex-col items-center max-w-screen-2xl mx-auto">
+      <div className="md:px-[120px] px-2 py-[96px] flex flex-col items-center max-w-screen-2xl mx-auto">
         <div className="max-w-[730px]">
-          <h2 className="font-semibold text-2xl space-y-2 [&>span]:block">
+          <h2 className="font-semibold md:text-2xl space-y-2 [&>span]:block">
             <span>Why Leading Businesses</span>
-            <span className="font-normal text-5xl font-gilda-display">
+            <span className="font-normal md:text-5xl text-[24px] font-gilda-display">
               Choose Magna Partners
             </span>
           </h2>
-          <p className="mt-5">
+          <p className="mt-5 md:text-[20]">
             These numbers are more than just figures, they represent our
             dedication to making a meaningful difference and driving progress.
             We continue to strive for excellence, aiming to create even greater
             impacts in the years to come.
           </p>
         </div>
-        <div className="rounded-xl w-full mt-[33px] border-[0.5px] py-[30px] px-[80px] border-white flex justify-between gap-5 bg-white/[8%] [&>div]:space-y-3 [&>div>h3]:font-bold [&>div>h3]:text-5xl">
-          <div>
-            <h3>
-              <CountUp
-                start={0}
-                end={35}
-                suffix="k+"
-                enableScrollSpy
-                scrollSpyOnce
-              />
-            </h3>
-            <p>Event Registrans</p>
-          </div>
-          <div>
-            <h3>
-              <CountUp
-                start={0}
-                end={150}
-                suffix="k+"
-                enableScrollSpy
-                scrollSpyOnce
-              />
-            </h3>
-            <p>Social Media Followers</p>
-          </div>
-          <div>
-            <h3>
-              <CountUp
-                start={0}
-                end={300}
-                suffix="+"
-                enableScrollSpy
-                scrollSpyOnce
-              />
-            </h3>
-            <p>Universities Reached</p>
-          </div>
-          <div>
-            <h3>
-              <CountUp
-                start={0}
-                end={500}
-                suffix="+"
-                enableScrollSpy
-                scrollSpyOnce
-              />
-            </h3>
-            <p>Partnerships</p>
-          </div>
-          <div>
-            <h3>
-              <CountUp
-                start={0}
-                end={30}
-                suffix="+"
-                enableScrollSpy
-                scrollSpyOnce
-              />
-            </h3>
-            <p>Company Partnerships</p>
+        <div className="rounded-xl w-full mt-[33px] border-[0.5px] py-[30px] px-4 md:px-[80px] border-white flex flex-wrap justify-between gap-5 bg-white/[8%]">
+          <div className="grid grid-cols-3 md:grid-cols-5 w-full gap-5 items-center justify-center">
+            <div className="flex flex-col items-center space-y-3">
+              <h3 className="font-bold text-2xl md:text-5xl">
+                <CountUp
+                  start={0}
+                  end={35}
+                  suffix="k+"
+                  enableScrollSpy
+                  scrollSpyOnce
+                />
+              </h3>
+              <p>Event Registrants</p>
+            </div>
+            <div className="flex flex-col items-center space-y-3">
+              <h3 className="font-bold text-2xl md:text-5xl">
+                <CountUp
+                  start={0}
+                  end={150}
+                  suffix="k+"
+                  enableScrollSpy
+                  scrollSpyOnce
+                />
+              </h3>
+              <p>Social Media Followers</p>
+            </div>
+            <div className="flex flex-col items-center space-y-3">
+              <h3 className="font-bold text-2xl md:text-5xl">
+                <CountUp
+                  start={0}
+                  end={300}
+                  suffix="+"
+                  enableScrollSpy
+                  scrollSpyOnce
+                />
+              </h3>
+              <p>Universities Reached</p>
+            </div>
+            <div className="flex flex-col items-center space-y-3">
+              <h3 className="font-bold text-2xl md:text-5xl">
+                <CountUp
+                  start={0}
+                  end={500}
+                  suffix="+"
+                  enableScrollSpy
+                  scrollSpyOnce
+                />
+              </h3>
+              <p>Partnerships</p>
+            </div>
+            <div className="flex flex-col items-center space-y-3">
+              <h3 className="font-bold text-2xl md:text-5xl">
+                <CountUp
+                  start={0}
+                  end={30}
+                  suffix="+"
+                  enableScrollSpy
+                  scrollSpyOnce
+                />
+              </h3>
+              <p>Company Partnerships</p>
+            </div>
           </div>
         </div>
       </div>
