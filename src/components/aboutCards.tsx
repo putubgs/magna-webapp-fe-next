6c4faf788026dad18a91eedc3ec52c03a897de52
alvinfdl21 <alvinfdl21@gmail.com>
2024-07-29 19:26:42 +0700

    fix: bu card responsiveness

--- src/components/aboutCards.tsx
+++ src/components/aboutCards.tsx
@@ -12,10 +12,10 @@ export const AboutCard = ({ abouts }: AboutCardProps) => (
   <>
     {abouts.map((about, index) => (
       <div
-        className="border border-[#FFFFFF33] w-[170px]  h-[287px] lg:w-[265px] lg:h-[353px] max-h-[400px] p-[6px]"
+        className="border border-[#FFFFFF33] w-[170px] lg:w-[265px] p-[6px]"
         key={index}
       >
-        <div className="h-[245px] lg:h-[307px]">
+        <div className="">
           <div className="relative w-full h-[80px] lg:h-[134px] bg-[#FFFFFF14]">
             <div className="flex justify-center h-full items-center ">
               <div
@@ -35,7 +35,7 @@ export const AboutCard = ({ abouts }: AboutCardProps) => (
               </div>
             </div>
           </div>
-          <div className="gap-2 lg:gap-[14px] w-[158px] lg:w-full h-[165px] lg:h-[173px] text-center">
+          <div className="gap-2 lg:gap-[14px] w-[158px] lg:w-full min-h-[165px] lg:min-h-[173px] text-center">
             <div className="h-[40px] my-[12px] flex items-center justify-center">
               <p className="text-sm lg:text-xl font-bold">{about.title}</p>
             </div>
@@ -45,7 +45,7 @@ export const AboutCard = ({ abouts }: AboutCardProps) => (
             </p>
           </div>
         </div>
-        <div className="w-[56px] mx-auto  flex gap-[20px]">
+        <div className="w-[56px] mb-2 mx-auto  flex gap-[20px]">
           <Link href={about.igLink} target="_blank">
             <InstaIcon size={18} color="#A3A3A3" />
           </Link>
