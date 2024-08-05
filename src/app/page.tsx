18a87ab82940a5b7bfcc6dd9400420e8a39e06a6
SeedONova <putubaguswidia@gmail.com>
2024-08-05 20:26:15 +0700

    change loading time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
