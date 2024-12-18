c6798deb403c34af82412d97bf4dc882ce192092
SeedONova <putubaguswidia@gmail.com>
2024-12-18 23:06:55 +0700

    metadata changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Magna Partners",
   description:
-    "Magna Partners was established in 2024 as a collectively developed network of organizations and communities that focused on solving the problems of youths: high schoolers, college students, and young professionals, with various types of products and services.",
+    "Magna Partners empowers youth through mentorship, skill development, and career guidance, fostering leadership, entrepreneurship, and career growth.",
 };
 
 export default function RootLayout({
