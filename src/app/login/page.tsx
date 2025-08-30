a332b91b2a84edbfe5ab75ba37718098c8cde027
gitaufar <zhafiraufar123@gmail.com>
2025-08-30 12:28:34 +0700

    feat/add login and forgot password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -28,9 +28,9 @@ function Login() {
 
   return (
     <div className="min-w-screen max-h-screen flex">
-      <div className="flex flex-col justify-center min-h-screen bg-[#171717] flex-1">
-        <div className="md:hidden block w-[273px] h-[291px] bg-[#3D0FA8] opacity-50 blur-3xl rounded-full absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-0"></div>
-        <div className="flex flex-col min-w-full h-full justify-center items-center p-10 z-10">
+      <div className="flex flex-col justify-center min-h-screen bg-[#171717] w-full md:w-[60%]">
+        <div className="md:hidden block w-[80%] h-[45%] bg-[#3D0FA8] opacity-50 blur-3xl rounded-full absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-0"></div>
+        <div className="flex flex-col min-w-full h-full justify-center items-center p-6 md:p-10 z-10">
           <div className="flex flex-col justify-center md:pt-20 md:pb-24 w-full max-w-7xl">
             <form onSubmit={submitHandler} className="flex flex-col gap-2">
               <p className="text-[40px] gilda-font text-white">Welcome Back!</p>
@@ -86,8 +86,8 @@ function Login() {
           </div>
         </div>
       </div>
-      <div className="pointer-events-none md:block hidden w-[40%] h-[100%] bg-[#3D0FA8] opacity-50 blur-3xl rounded-full absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2"></div>
-      <div className="md:flex hidden bg-black w-full items-center justify-center flex-1 z-10">
+      <div className="pointer-events-none md:block hidden w-[40%] h-[100%] bg-[#3D0FA8] opacity-50 blur-3xl rounded-full absolute top-[50%] left-[60%] -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="md:flex hidden bg-black w-[40%] items-center justify-center z-10">
         <Image
           src="/assets/logo/business-units/Magna.png"
           alt="Magna Logo"
