8413fa723cafa04f14b96f27a8e8f39285096ecb
gitaufar <zhafiraufar123@gmail.com>
2025-10-06 19:57:56 +0700

    feat(impact): add impact api (super-admin not tested yet)

---
+++ src/app/api/orgimpacts/[impact_id]/route.ts
@@ -0,0 +1,115 @@
+import { AuthenticatedRequest, withAuth } from "@/utils/authMiddleware";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+// GET OrgImpact by ID
+export const GET = withAuth(
+  async (
+    req: AuthenticatedRequest,
+    context: { params: { impact_id: string } }
+  ) => {
+    try {
+      const { impact_id } = context.params;
+      const supabase = createClient(cookies());
+      const { data, error } = await supabase
+        .from("org_impacts")
+        .select("*")
+        .eq("impact_id", impact_id)
+        .single();
+      if (error) {
+        return NextResponse.json(
+          { message: "Error getting org_impacts", details: error.message },
+          { status: 500 }
+        );
+      }
+      return NextResponse.json(
+        {
+          message: `OrgImpact ${impact_id} fetched successfully`,
+          data: data,
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Error getting org_impacts" },
+        { status: 500 }
+      );
+    }
+  },
+  "super-admin"
+);
+
+// Update OrgImpact by ID#
+export const PUT = withAuth(
+  async (
+    req: AuthenticatedRequest,
+    context: { params: { impact_id: string } }
+  ) => {
+    try {
+      const { impact_id } = context.params;
+      const body = await req.json();
+      const supabase = createClient(cookies());
+      const { data, error } = await supabase
+        .from("org_impacts")
+        .update(body)
+        .eq("impact_id", impact_id)
+        .select()
+        .single();
+      if (error) {
+        return NextResponse.json(
+          { message: "Error updating org_impacts", details: error.message },
+          { status: 500 }
+        );
+      }
+      return NextResponse.json(
+        {
+          message: `OrgImpact ${impact_id} updated successfully`,
+          data: data,
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Error updating org_impacts" },
+        { status: 500 }
+      );
+    }
+  },
+  "super-admin"
+);
+
+// DELETE OrgImpact by ID
+export const DELETE = withAuth(
+  async (
+    req: AuthenticatedRequest,
+    context: { params: { impact_id: string } }
+  ) => {
+    try {
+      const { impact_id } = context.params;
+      const supabase = createClient(cookies());
+      const { error } = await supabase
+        .from("org_impacts")
+        .delete()
+        .eq("impact_id", impact_id);
+      if (error) {
+        return NextResponse.json(
+          { message: "Error deleting org_impacts", details: error.message },
+          { status: 500 }
+        );
+      }
+      return NextResponse.json(
+        {
+          message: `OrgImpact ${impact_id} deleted successfully`,
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Error deleting org_impacts" },
+        { status: 500 }
+      );
+    }
+  },
+  "super-admin"
+);
---
+++ src/app/api/orgimpacts/route.ts
@@ -0,0 +1,68 @@
+import { AuthenticatedRequest, withAuth } from "@/utils/authMiddleware";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+// CREATE OrgImpact
+export const POST = withAuth(
+    async (req: AuthenticatedRequest) => {
+        try {
+            const supabase = createClient(cookies());
+
+            const body = await req.json();
+            const { data, error } = await supabase
+                .from("org_impacts")
+                .insert([body])
+                .select()
+                .single();
+
+            if (error) {
+                return NextResponse.json(
+                    { message: "Error creating orgimpact", details: error.message },
+                    { status: 500 }
+                );
+            }
+
+            return NextResponse.json(
+                {
+                    message: "OrgImpact created successfully",
+                    data: data,
+                },
+                { status: 200 }
+            );
+        } catch (error) {
+            return NextResponse.json(
+                { message: "Error creating orgimpact" },
+                { status: 500 }
+            );
+        }
+    }, "super-admin"
+);
+
+// GET All OrgImpacts
+export const GET = withAuth(async (req: AuthenticatedRequest) => {
+    try {
+        const supabase = createClient(cookies());
+        const { data, error } = await supabase
+            .from("org_impacts")
+            .select("*")
+            .order("created_at", { ascending: false });
+
+        if (error) {
+            return NextResponse.json(
+                { message: "Error getting orgimpacts", details: error.message },
+                { status: 500 }
+            );
+        }
+
+        return NextResponse.json(
+            { message: "OrgImpacts retrieved successfully", data: data },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Error getting orgimpacts" },
+            { status: 500 }
+        );
+    }
+}, "super-admin");
